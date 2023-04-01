aa8c84f0eb527a1f3e893fc3d0122a5856aa5f2f
Akhil <sharma.akhil484@gmail.com>
2023-04-01 19:57:45 +0530

    configured DB

---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+
+        res = calc.add(5,7)
+
+        self.assertEqual(res,12)
+
+    def test_subtract_numbers(self):
+        """Subtracting Numbers"""
+
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res,5)
